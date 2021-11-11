02a57d360a8d4ce325a9aa76e61ba0e17be8d70f
abdulvokhid-ochilov <asliddin00buriev@gmail.com>
2021-11-11 12:26:28 +0900

    testing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
   res.header("Access-Control-Allow-Methods", "GET, PATCH, POST");
-  next();
+  next()
 })
 
 
